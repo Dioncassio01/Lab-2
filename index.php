729cb593c75716e2b36e0aaa1dd78e37fa87c7a6
Dioncassio01 <dioncassio01@hotmail.com>
2013-10-16 14:02:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+//error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 require_once 'HTMLPage.php';
 require_once 'login.php';
 
 session_start();
+
+if (isset($_SESSION["session_test"]) == FALSE) {
+	$_SESSION["session_test"] = array();
+	$_SESSION["session_test"]["webbläsare"] = $_SERVER["HTTP_USER_AGENT"];
+	$_SESSION["session_test"]["ip"] = $_SERVER["REMOTE_ADDR"];
+}
+if ($_SESSION["session_test"]["webbläsare"] != $_SERVER["HTTP_USER_AGENT"]) {
+	echo "sessiontjuv !!!";
+	//$_SESSION["session_test"]["felaktig session"]=TRUE;
+}
+if ($_SESSION["session_test"]["ip"] != $_SERVER["REMOTE_ADDR"]) {
+	echo "sessiontjuv ip!!!";
+}
+
+var_dump($_COOKIE);
+
+if (isset($_COOKIE["somecookie"])) {
+	//är det giltig cooki?
+	$cookieEndTime = file_get_contents("endtime.text");
+	echo "$cookieEndTime";
+
+	if (time() > $cookieEndTime) {
+		echo "cookie is to old";
+	} else {
+		echo "Cookie is fine";
+	}
+
+}
+$endtime = time() + 10;
+file_put_contents("$enttime.txt", "$endtime");
+setcookie("somecookie", "", time() + 10);
+//var_dump($_SERVER);
 
 /**
  * @var userName (HARD CODED) In a real (not hard coded)is need a database
@@ -24,48 +56,33 @@ $login = new \model\login();
 if (isset($_GET["logout"])) {
 	$login -> doLogout();
 } else {
-	if (isset($_SESSION[\model\login::LoggedIn]) && $_SESSION[\model\login::LoggedIn] == TRUE) {
+	if (isset($_SESSION[\model\login::LoggedIn])) {
 		echo $login -> doLogin();
 
 	} else {
 		//rätt användaruppgifter
 		if ($login -> controller()) {
-						
-			if($login -> getuserName() == $userName && $login -> getPassword() == $password && $login->checkAutologin()){
-				echo $login-> doCheckbox();
-			 	
-			}elseif ($login -> getuserName() == $userName && $login -> getPassword() == $password) {
+
+			if ($login -> getuserName() == $userName && $login -> getPassword() == $password && $login -> checkAutologin()) {
+				echo $login -> doCheckbox();
+			} elseif ($login -> getuserName() == $userName && $login -> getPassword() == $password) {
 				echo $login -> doLogin();
-				//TODO:fix it
-			}elseif ($login ->getuserName()==""){
-				echo $login->doCheckusername();
-			}elseif ($login ->getPassword()== ""){
-				echo $login->doCheckPassword();
-			}
-			else {
+			} elseif ($login -> getuserName() == "") {
+				echo $login -> doCheckusername();
+			} elseif ($login -> getPassword() == "") {
+				echo $login -> doCheckPassword();
+			} else {
 				//if (!$login -> checkUserName($login -> getuserName()) && !$login -> checkPassword($login -> getPassword()))
-					echo $login -> doCorrectLogging();
+				echo $login -> doCorrectLogging();
 				//fel använddaruppgifter
 			}
 		} else {
-			//TODO::Look up why don't show the first page
-			//hämta inloggningsformulär
-			echo $login -> getform();
+			//inloggning med cookies
+			if (!isset($_SESSION[\model\login::LoggedIn]) && isset($_COOKIE['userNamecookie'])) {
+				echo $login -> dosavedCookies();
+			} else
+				//hämta inloggningsformulär
+				echo $login -> doloadPage();
 		}
 	}
 }
-
-//echo '<hr /><h2>SESSION</h2><pre>';
-//var_dump($_SESSION);
-//echo '</pre>';
-
-//echo '<h2>$_POST</h2>';
-//echo '<pre>';
-//var_dump($_POST);
-//echo '</pre>';
-
-//echo '<h2>$_GET</h2>';
-//echo '<pre>';
-//var_dump($_GET);
-//echo '</pre>';
-?>
